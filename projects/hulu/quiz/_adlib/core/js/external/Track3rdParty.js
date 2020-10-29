78defa247eebb2d429d24f41b3a596a562dfad3e
Jacques Altounian <jacquesaltounian@Jacquess-MacBook-Pro.local>
2020-10-28 17:49:10 -0700

    switch to CP files for hulu quiz

--- projects/hulu/quiz/_adlib/core/js/external/Track3rdParty.js
+++
@@ -1,8 +0,0 @@
-var Track3rdParty = new function(){
-
-	this.pixel = function ( url ){
-		trace ( 'Track3rdParty.pixel()\n\turl =', url )
-		var img = new Image();
-		img.src = url;
-	}
-}
--- projects/hulu/quiz/_adlib/core/js/net/RemoteControl.js
+++
@@ -1,2 +0,0 @@
-// THIS IS THE DEPRECATED RemoteControl.js, as of AdApp v4.4.12 - 2/29/2016
-var RC={debug:true};
