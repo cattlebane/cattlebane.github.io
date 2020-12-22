b283d1f09f7ee673f06dc24697e26ef3d080be1d
Jacques Altounian <jacquesaltounian@Jacquess-MacBook-Pro.local>
2020-12-22 09:38:56 -0800

    revert test changes

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -4,7 +4,7 @@ import { Content } from 'carbon-components-react/lib/components/UIShell'
 export const StyledContent = styled(Content)`
   min-height: 100vh;
 
-  @media (max-width: 630px) {
+  @media (max-width: 640px) {
     margin-left: 0 !important;
   }
 `
